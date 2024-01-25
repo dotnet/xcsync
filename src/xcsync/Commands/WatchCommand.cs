143ab0951f937044101c2172828bb7a9ad30e995
Haritha Mohan <harithamohan@microsoft.com>
2024-01-25 13:31:18 -0800

    [xcsync] update path options (#2744)
    ensure that a csproj file is passed for the project option and shifting
    to make behavior similar to what happens when a command like dotnet
    build is used.
    
    Contributes to https://github.com/xamarin/maccore/issues/2728

--- src/xcsync/Commands/WatchCommand.cs
+++ src/xcsync/Commands/WatchCommand.cs
@@ -1,7 +1,7 @@
 namespace xcsync.Commands;
 
 public class WatchCommand {
-	public static void Execute (DirectoryInfo project, DirectoryInfo target, bool force)
+	public static void Execute (string project, string target, bool force)
 	{
 		Console.WriteLine ($"Watching files from project '{project}' to target '{target}'");
 		// Implement logic here
