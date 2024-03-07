e94a3903bfed24dad42bccc7f31ee9c53b48d662
Haritha Mohan <harithamohan@microsoft.com>
2024-03-06 17:13:26 -0800

    [xcsync] process outlets and actions (#2760)
    gather data regarding outlets and actions in designer files to prepare for Xcode project generation

--- src/xcsync/Projects/NSObject.cs
+++ src/xcsync/Projects/NSObject.cs
@@ -2,15 +2,34 @@
 
 namespace xcsync.Projects;
 
-public class NSObject (string cliName, string objcName, NSObject? baseType, bool isModel, bool inDesigner) {
+public class NSObject (string cliName, string objcName, NSObject? baseType, bool isModel, bool inDesigner, List<IBOutlet>? outlets, List<IBAction>? actions) {
 
 	public string CliType { get; set; } = cliName;
 	public string ObjCType { get; set; } = objcName;
 	public NSObject? BaseType { get; set; } = baseType;
 	public bool IsModel { get; set; } = isModel;
 	public bool InDesigner { get; set; } = inDesigner;
+	public List<IBOutlet>? Outlets { get; set; } = outlets;
+	public List<IBAction>? Actions { get; set; } = actions;
+}
 
-	// todo: implement in next PR
-	// List<IBOutlet> Outlets { get; }
-	// List<IBAction> Actions { get; }
+public class IBOutlet (string cliName, string objcName, string cliType, string? objcType, bool isCollection) {
+	public string CliName { get; set; } = cliName;
+	public string ObjCName { get; set; } = objcName;
+	public string CliType { get; set; } = cliType;
+	public string? ObjCType { get; set; } = objcType;
+	public bool IsCollection { get; set; } = isCollection;
+}
+
+public class IBAction (string cliName, string objcName, List<IBActionParameter> parameters) {
+	public string CliName { get; set; } = cliName;
+	public string ObjCName { get; set; } = objcName;
+	public List<IBActionParameter> Parameters { get; set; } = parameters;
+}
+
+public class IBActionParameter (string cliName, string? objcName, string cliType, string? objcType) {
+	public string CliName { get; set; } = cliName;
+	public string? ObjCName { get; set; } = objcName;
+	public string CliType { get; set; } = cliType;
+	public string? ObjCType { get; set; } = objcType;
 }
