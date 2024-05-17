24cec298d8551e8838ce885cc8bc9f3f56d2cb46
Michael Cummings (MSFT) <mcumming@microsoft.com>
2024-05-17 11:47:23 -0400

    [xcsync] Get *all* TFMs from the project file (#2808)

--- test/xcsync.tests/Commands/CommandValidationTests.cs
+++ test/xcsync.tests/Commands/CommandValidationTests.cs
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation.  All rights reserved.
 
-using System.IO.Abstractions;
 using System.IO.Abstractions.TestingHelpers;
 using Xamarin;
 using xcsync.Commands;
@@ -41,81 +40,9 @@ public class CommandValidationTests (ITestOutputHelper TestOutput) : Base {
 	public void BaseCommandValidation_SingleProject (string projectType, string tfm, string targetPath, string expectedError)
 	{
 		var projectTypes = new Dictionary<string, string> {
-			{ "ios", @"
-<Project Sdk=""Microsoft.NET.Sdk"">
-  <PropertyGroup>
-	<TargetFramework>net8.0-ios</TargetFramework>
-	<OutputType>Exe</OutputType>
-	<Nullable>enable</Nullable>
-	<ImplicitUsings>true</ImplicitUsings>
-	<SupportedOSPlatformVersion>13.0</SupportedOSPlatformVersion>
-  </PropertyGroup>
-</Project>
-			" },
-			{"macos", @"
-<Project Sdk=""Microsoft.NET.Sdk"">
-	<PropertyGroup>
-		<TargetFramework>net8.0-macos</TargetFramework>
-		<RootNamespace>test_project</RootNamespace>
-		<OutputType>Exe</OutputType>
-		<Nullable>enable</Nullable>
-		<ImplicitUsings>true</ImplicitUsings>
-		<SupportedOSPlatformVersion>10.15</SupportedOSPlatformVersion>
-	</PropertyGroup>
-</Project>" },
-			{ "maui", @"
-<Project Sdk=""Microsoft.NET.Sdk"">
-	<PropertyGroup>
-		<TargetFrameworks>net8.0-android;net8.0-ios;net8.0-maccatalyst</TargetFrameworks>
-		<TargetFrameworks Condition=""$([MSBuild]::IsOSPlatform('windows'))"">$(TargetFrameworks);net8.0-windows10.0.19041.0</TargetFrameworks>
-		<!-- Uncomment to also build the tizen app. You will need to install tizen by following this: https://github.com/Samsung/Tizen.NET -->
-		<!-- <TargetFrameworks>$(TargetFrameworks);net8.0-tizen</TargetFrameworks> -->
-		<!-- Note for MacCatalyst:
-			The default runtime is maccatalyst - x64, except in Release config, in which case the default is maccatalyst - x64; maccatalyst - arm64.
-			When specifying both architectures, use the plural <RuntimeIdentifiers> instead of the singular <RuntimeIdentifier>.
-			The Mac App Store will NOT accept apps with ONLY maccatalyst - arm64 indicated;
-			either BOTH runtimes must be indicated or ONLY macatalyst-x64. -->
-		<!--For example: <RuntimeIdentifiers>maccatalyst-x64; maccatalyst-arm64</RuntimeIdentifiers> -->
-		<OutputType> Exe </OutputType>
-		<RootNamespace> MauiTest </RootNamespace>
-		<UseMaui> true </UseMaui>
-		<SingleProject> true </SingleProject>
-		<ImplicitUsings> enable </ImplicitUsings>
-		<Nullable> enable </Nullable>
-		<!--Display name-->
-		<ApplicationTitle> MauiTest </ApplicationTitle>
-		<!--App Identifier-->
-		<ApplicationId> com.companyname.mauitest </ApplicationId>
-		<!--Versions-->
-		<ApplicationDisplayVersion> 1.0 </ApplicationDisplayVersion>
-		<ApplicationVersion > 1 </ApplicationVersion>
-		<SupportedOSPlatformVersion Condition = ""$([MSBuild]::GetTargetPlatformIdentifier('$(TargetFramework)')) == 'ios'"" > 11.0 </SupportedOSPlatformVersion>
-		<SupportedOSPlatformVersion Condition = ""$([MSBuild]::GetTargetPlatformIdentifier('$(TargetFramework)')) == 'maccatalyst'"" > 13.1 </SupportedOSPlatformVersion>
-		<SupportedOSPlatformVersion Condition = ""$([MSBuild]::GetTargetPlatformIdentifier('$(TargetFramework)')) == 'android'"" > 21.0 </SupportedOSPlatformVersion>
-		<SupportedOSPlatformVersion Condition = ""$([MSBuild]::GetTargetPlatformIdentifier('$(TargetFramework)')) == 'windows'"" > 10.0.17763.0 </SupportedOSPlatformVersion>
-		<TargetPlatformMinVersion Condition = ""$([MSBuild]::GetTargetPlatformIdentifier('$(TargetFramework)')) == 'windows'"" > 10.0.17763.0 </TargetPlatformMinVersion>
-		<SupportedOSPlatformVersion Condition = ""$([MSBuild]::GetTargetPlatformIdentifier('$(TargetFramework)')) == 'tizen'"" > 6.5 </SupportedOSPlatformVersion>
-	</PropertyGroup>
-	<ItemGroup>
-		<!--App Icon-->
-		<MauiIcon Include = ""Resources\AppIcon\appicon.svg"" ForegroundFile = ""Resources\AppIcon\appiconfg.svg"" Color = ""#512BD4"" />
-		<!--Splash Screen-->
-		<MauiSplashScreen Include = ""Resources\Splash\splash.svg"" Color = ""#512BD4"" BaseSize = ""128,128"" />
-		<!--Images-->
-		<MauiImage Include = ""Resources\Images\*"" />
-		<MauiImage Update = ""Resources\Images\dotnet_bot.png"" Resize = ""True"" BaseSize = ""300,185"" />
-		<!--Custom Fonts-->
-		<MauiFont Include = ""Resources\Fonts\*"" />
-		<!--Raw Assets (also remove the ""Resources\Raw"" prefix)-->
-		<MauiAsset Include = ""Resources\Raw\**"" LogicalName = ""%(RecursiveDir)%(Filename)%(Extension)"" />
-	</ItemGroup>
-	<ItemGroup>
-		<PackageReference Include = ""Microsoft.Maui.Controls"" Version = ""$(MauiVersion)"" />
-		<PackageReference Include = ""Microsoft.Maui.Controls.Compatibility"" Version = ""$(MauiVersion)"" />
-		<PackageReference Include = ""Microsoft.Extensions.Logging.Debug"" Version = ""9.0.0-preview.1.24080.9"" />
-	</ItemGroup>
-</Project>
-		" }
+			{ "ios", net_8_0_iosProject },
+			{"macos", net_8_0_macosProject},
+			{ "maui", mauiProject }
 		};
 
 		var projectName = Guid.NewGuid ().ToString ();
@@ -228,5 +155,121 @@ public class CommandValidationTests (ITestOutputHelper TestOutput) : Base {
 			Assert.True (fileSystem.Directory.Exists (validation.TargetPath));
 		}
 	}
+
+	[Theory]
+	[InlineData (net_8_0_macosProject, new string [] { "net8.0-macos" })]
+	[InlineData (mauiProject, new string [] { "net8.0-android", "net8.0-ios", "net8.0-maccatalyst", "net8.0-windows10.0.19041.0" })]
+	public void BaseCommandValidation_GetTFMs (string csProject, string [] expectedTfms)
+	{
+		var projectName = Guid.NewGuid ().ToString ();
+		var projectDir = "/src";
+
+		var fileSystem = new MockFileSystem (
+			new Dictionary<string, MockFileData> {
+				{ Path.Combine (projectDir, $"{projectName}.csproj"), new MockFileData (csProject) },
+			}
+		);
+
+		var baseCommand = new BaseCommand<string> (fileSystem, "test", "test description");
+
+		baseCommand.TryGetTfmFromProject (Path.Combine (projectDir, $"{projectName}.csproj"), out var tfms);
+
+		Assert.NotNull (tfms);
+		Assert.NotEmpty (tfms);
+		Assert.Equivalent (expectedTfms, tfms);
+	}
+
+	const string net_8_0_iosProject = @"
+<Project Sdk=""Microsoft.NET.Sdk"">
+  <PropertyGroup>
+	<TargetFramework>net8.0-ios</TargetFramework>
+	<OutputType>Exe</OutputType>
+	<Nullable>enable</Nullable>
+	<ImplicitUsings>true</ImplicitUsings>
+	<SupportedOSPlatformVersion>13.0</SupportedOSPlatformVersion>
+  </PropertyGroup>
+</Project>
+";
+	const string net_8_0_macosProject = @"
+<Project Sdk=""Microsoft.NET.Sdk"">
+    <PropertyGroup>
+        <TargetFramework>net8.0-macos</TargetFramework>
+        <RootNamespace>test_project</RootNamespace>
+        <OutputType>Exe</OutputType>
+        <Nullable>enable</Nullable>
+        <ImplicitUsings>true</ImplicitUsings>
+        <SupportedOSPlatformVersion>10.15</SupportedOSPlatformVersion>
+    </PropertyGroup>
+</Project>
+";
+	const string mauiProject = @"
+<Project Sdk=""Microsoft.NET.Sdk"">
+
+	<PropertyGroup>
+		<TargetFrameworks>net8.0-android;net8.0-ios;net8.0-maccatalyst</TargetFrameworks>
+		<TargetFrameworks Condition=""$([MSBuild]::IsOSPlatform('windows'))"">$(TargetFrameworks);net8.0-windows10.0.19041.0</TargetFrameworks>
+		<!-- Uncomment to also build the tizen app. You will need to install tizen by following this: https://github.com/Samsung/Tizen.NET -->
+		<!-- <TargetFrameworks>$(TargetFrameworks);net8.0-tizen</TargetFrameworks> -->
+
+		<!-- Note for MacCatalyst:
+		The default runtime is maccatalyst-x64, except in Release config, in which case the default is maccatalyst-x64;maccatalyst-arm64.
+		When specifying both architectures, use the plural <RuntimeIdentifiers> instead of the singular <RuntimeIdentifier>.
+		The Mac App Store will NOT accept apps with ONLY maccatalyst-arm64 indicated;
+		either BOTH runtimes must be indicated or ONLY macatalyst-x64. -->
+		<!-- For example: <RuntimeIdentifiers>maccatalyst-x64;maccatalyst-arm64</RuntimeIdentifiers> -->
+
+		<OutputType>Exe</OutputType>
+		<RootNamespace>MauiTest</RootNamespace>
+		<UseMaui>true</UseMaui>
+		<SingleProject>true</SingleProject>
+		<ImplicitUsings>enable</ImplicitUsings>
+		<Nullable>enable</Nullable>
+
+		<!-- Display name -->
+		<ApplicationTitle>MauiTest</ApplicationTitle>
+
+		<!-- App Identifier -->
+		<ApplicationId>com.companyname.mauitest</ApplicationId>
+
+		<!-- Versions -->
+		<ApplicationDisplayVersion>1.0</ApplicationDisplayVersion>
+		<ApplicationVersion>1</ApplicationVersion>
+
+		<SupportedOSPlatformVersion Condition=""$([MSBuild]::GetTargetPlatformIdentifier('$(TargetFramework)')) == 'ios'"">11.0</SupportedOSPlatformVersion>
+
+		<SupportedOSPlatformVersion Condition=""$([MSBuild]::GetTargetPlatformIdentifier('$(TargetFramework)')) == 'maccatalyst'"">13.1</SupportedOSPlatformVersion>
+		<SupportedOSPlatformVersion Condition=""$([MSBuild]::GetTargetPlatformIdentifier('$(TargetFramework)')) == 'android'"">21.0</SupportedOSPlatformVersion>
+		<SupportedOSPlatformVersion Condition=""$([MSBuild]::GetTargetPlatformIdentifier('$(TargetFramework)')) == 'windows'"">10.0.17763.0</SupportedOSPlatformVersion>
+		<TargetPlatformMinVersion Condition=""$([MSBuild]::GetTargetPlatformIdentifier('$(TargetFramework)')) == 'windows'"">10.0.17763.0</TargetPlatformMinVersion>
+		<SupportedOSPlatformVersion Condition=""$([MSBuild]::GetTargetPlatformIdentifier('$(TargetFramework)')) == 'tizen'"">6.5</SupportedOSPlatformVersion>
+	</PropertyGroup>
+
+	<ItemGroup>
+		<!-- App Icon -->
+		<MauiIcon Include=""Resources\AppIcon\appicon.svg"" ForegroundFile=""Resources\AppIcon\appiconfg.svg"" Color=""#512BD4"" />
+
+		<!-- Splash Screen -->
+		<MauiSplashScreen Include=""Resources\Splash\splash.svg"" Color=""#512BD4"" BaseSize=""128,128"" />
+
+		<!-- Images -->
+		<MauiImage Include=""Resources\Images\*"" />
+		<MauiImage Update=""Resources\Images\dotnet_bot.png"" Resize=""True"" BaseSize=""300,185"" />
+
+		<!-- Custom Fonts -->
+		<MauiFont Include=""Resources\Fonts\*"" />
+
+		<!-- Raw Assets (also remove the ""Resources\Raw"" prefix) -->
+		<MauiAsset Include=""Resources\Raw\**"" LogicalName=""%(RecursiveDir)%(Filename)%(Extension)"" />
+	</ItemGroup>
+
+	<ItemGroup>
+		<PackageReference Include=""Microsoft.Maui.Controls"" Version=""$(MauiVersion)"" />
+		<PackageReference Include=""Microsoft.Maui.Controls.Compatibility"" Version=""$(MauiVersion)"" />
+		<PackageReference Include=""Microsoft.Extensions.Logging.Debug"" Version=""9.0.0-preview.1.24080.9"" />
+	</ItemGroup>
+
+</Project>
+";
+
 
 }
