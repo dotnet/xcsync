44ddf1c26cd679bd42e2ecb1054af159039fade8
Michael Cummings (MSFT) <mcumming@microsoft.com>
2024-05-31 14:28:17 -0400

    Add ITypeSymbol reference to TypeMapping (#2820)

--- test/xcsync.tests/Projects/TypeServiceTest.cs
+++ test/xcsync.tests/Projects/TypeServiceTest.cs
@@ -1,3 +1,5 @@
+using Microsoft.CodeAnalysis;
+using Moq;
 using xcsync.Projects;
 
 namespace xcsync.tests.Projects;
@@ -8,15 +10,16 @@ public class TypeServiceTest {
 	public TypeServiceTest ()
 	{
 		typeService = new TypeService ();
-		typeService.AddType ("ViewController", "ViewController", null, false, false, false, null, null, new HashSet<string> ());
-		typeService.AddType ("AppDelegate", "AppDelegate", null, false, false, false, null, null, new HashSet<string> ());
-		typeService.AddType ("TypeExtension", "TypeExtension", null, false, false, false, null, null, new HashSet<string> ());
+
+		typeService.AddType (Mock.Of<INamedTypeSymbol> (),"ViewController", "ViewController", null, false, false, false, null, null, []);
+		typeService.AddType (Mock.Of<INamedTypeSymbol> (), "AppDelegate", "AppDelegate", null, false, false, false, null, null, []);
+		typeService.AddType (Mock.Of<INamedTypeSymbol> (), "TypeExtension", "TypeExtension", null, false, false, false, null, null, []);
 	}
 
 	[Fact]
 	public void AddType ()
 	{
-		var type = new TypeMapping ("NewType", "NewType", null, false, false, false, null, null, new HashSet<string> ());
+		var type = new TypeMapping (Mock.Of<INamedTypeSymbol> (), "NewType", "NewType", null, false, false, false, null, null, []);
 		var result = typeService.AddType (type);
 		Assert.NotNull (result);
 		Assert.Equal (type, result);
@@ -25,7 +28,7 @@ public class TypeServiceTest {
 	[Fact]
 	public void QueryExistentTypes ()
 	{
-		var type = new TypeMapping ("NewTypeCli", "NewTypeObjC", null, false, false, false, null, null, new HashSet<string> ());
+		var type = new TypeMapping (Mock.Of<INamedTypeSymbol> (), "NewTypeCli", "NewTypeObjC", null, false, false, false, null, null, []);
 		typeService.AddType (type);
 		var result = typeService.QueryTypes (objcType: "NewTypeObjC");
 		Assert.Single (result);
@@ -49,7 +52,7 @@ public class TypeServiceTest {
 	[Fact]
 	public void AddDuplicateType ()
 	{
-		var viewControllerDupe = new TypeMapping ("ViewController", "ViewController", null, false, false, false, null, null, new HashSet<string> ());
+		var viewControllerDupe = new TypeMapping (Mock.Of<INamedTypeSymbol> (), "ViewController", "ViewController", null, false, false, false, null, null, []);
 		typeService.AddType (viewControllerDupe);
 		var result = typeService.AddType (viewControllerDupe);
 		Assert.Null (result);
@@ -58,7 +61,7 @@ public class TypeServiceTest {
 	[Fact]
 	public void AddDuplicateObjCType ()
 	{
-		var appDelegateDupe = new TypeMapping ("NewAppDelegate", "AppDelegate", null, false, false, false, null, null, new HashSet<string> ());
+		var appDelegateDupe = new TypeMapping (Mock.Of<INamedTypeSymbol> (), "NewAppDelegate", "AppDelegate", null, false, false, false, null, null, []);
 		typeService.AddType (appDelegateDupe);
 		var result = typeService.AddType (appDelegateDupe);
 		Assert.Null (result);
