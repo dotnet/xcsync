f894804a23ce80e1e47be59c82ff8b98b155161a
Michael Cummings (MSFT) <mcumming@microsoft.com>
2024-05-31 14:16:06 -0400

    Walker and Visitor patterns for the Clang AST (#2819)

---
+++ src/xcsync/Ast/AstWalker.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+
+using ClangSharp;
+
+namespace xcsync;
+
+/// <summary>
+/// Represents a walker for the Clang AST.
+/// </summary>
+class AstWalker : IWalker<Cursor, IVisitor<Cursor>>
+{
+	/// <summary>
+	/// Walks the Clang AST.
+	/// </summary>
+	/// <param name="node">The node to walk.</param>
+	/// <param name="visitor">The visitor to use.</param>
+	/// <param name="filter">An optional filter to apply to the nodes.</param>
+	public void Walk(Cursor node, IVisitor<Cursor> visitor, Func<Cursor, bool>? filter = null)
+	{
+		visitor.Visit(node);
+		var children = node.CursorChildren;
+		foreach (var child in node.CursorChildren)
+		{
+			if (filter == null || filter(child))
+			{
+				Walk(child, visitor);
+			}
+		}
+	}
+}
---
+++ src/xcsync/Ast/IWalker.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+
+namespace xcsync;
+
+/// <summary>
+/// Represents a walker that traverses a tree-like structure and applies a visitor to each node.
+/// </summary>
+/// <typeparam name="TNode">The type of the nodes in the tree.</typeparam>
+/// <typeparam name="TVisitor">The type of the visitor.</typeparam>
+interface IWalker<TNode, TVisitor> {
+	/// <summary>
+	/// Walks the specified node and applies the visitor to each node in the tree.
+	/// </summary>
+	/// <param name="node">The root node of the tree.</param>
+	/// <param name="visitor">The visitor to apply to each node.</param>
+	/// <param name="filter">An optional filter function to determine which nodes to visit.</param>
+	void Walk (TNode node, TVisitor visitor, Func<TNode, bool>? filter);
+}
