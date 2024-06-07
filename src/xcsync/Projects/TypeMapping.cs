668206c5e6c829488970b8f6fb9b5460885f9c50
Haritha Mohan <harithamohan@microsoft.com>
2024-06-07 11:56:42 -0700

    [xcsync] Dotnet --> CLR (#2830)
    makes working with other updates a bit more clear :)
    
    also readied init code in ContinuousSyncContext for offloading async
    tasks to channels
    
    ---------
    
    Co-authored-by: Michael Cummings <mcumming@microsoft.com>

--- src/xcsync/Projects/TypeMapping.cs
+++ src/xcsync/Projects/TypeMapping.cs
@@ -4,26 +4,26 @@ using Microsoft.CodeAnalysis;
 
 namespace xcsync.Projects;
 
-record TypeMapping (INamedTypeSymbol TypeSymbol, string CliType, string ObjCType, TypeMapping? BaseType, bool IsModel, bool IsProtocol,
+record TypeMapping (INamedTypeSymbol TypeSymbol, string ClrType, string ObjCType, TypeMapping? BaseType, bool IsModel, bool IsProtocol,
 	bool InDesigner, List<IBOutlet>? Outlets, List<IBAction>? Actions, HashSet<string> References);
 
-class IBOutlet (string cliName, string objcName, string cliType, string? objcType, bool isCollection) {
-	public string CliName { get; set; } = cliName;
+class IBOutlet (string clrName, string objcName, string clrType, string? objcType, bool isCollection) {
+	public string ClrName { get; set; } = clrName;
 	public string ObjCName { get; set; } = objcName;
-	public string CliType { get; set; } = cliType;
+	public string ClrType { get; set; } = clrType;
 	public string? ObjCType { get; set; } = objcType;
 	public bool IsCollection { get; set; } = isCollection;
 }
 
-class IBAction (string cliName, string objcName, List<IBActionParameter> parameters) {
-	public string CliName { get; set; } = cliName;
+class IBAction (string clrName, string objcName, List<IBActionParameter> parameters) {
+	public string ClrName { get; set; } = clrName;
 	public string ObjCName { get; set; } = objcName;
 	public List<IBActionParameter> Parameters { get; set; } = parameters;
 }
 
-class IBActionParameter (string cliName, string? objcName, string cliType, string? objcType) {
-	public string CliName { get; set; } = cliName;
+class IBActionParameter (string clrName, string? objcName, string clrType, string? objcType) {
+	public string ClrName { get; set; } = clrName;
 	public string? ObjCName { get; set; } = objcName;
-	public string CliType { get; set; } = cliType;
+	public string ClrType { get; set; } = clrType;
 	public string? ObjCType { get; set; } = objcType;
 }
