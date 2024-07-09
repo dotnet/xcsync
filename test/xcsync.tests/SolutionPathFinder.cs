0c256ddc71fc387039eee3a15131c63679c6f971
Haritha Mohan <harithamohan@microsoft.com>
2024-07-09 08:26:32 -0700

    [xcsync] Dynamically resolve paths for test resource files (#2874)

---
+++ test/xcsync.tests/SolutionPathFinder.cs
@@ -0,0 +1,27 @@
+using System.Reflection;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+public static class SolutionPathFinder {
+	public static string GetProjectRoot () => Path.GetDirectoryName (GetSolutionPath ())!;
+
+	public static string GetSolutionPath ()
+    {
+        var assemblyDirectory = Path.GetDirectoryName (Assembly.GetExecutingAssembly ().Location)!;
+        // Navigate up directories until .sln file is found
+        return FindSolutionPath (assemblyDirectory);
+	}
+
+    public static string FindSolutionPath (string startDirectory)
+    {
+        // Check if root of filesystem has been reached
+        if (string.IsNullOrEmpty (startDirectory) || Directory.GetParent (startDirectory) == null)
+            throw new FileNotFoundException ("Solution file (.sln) not found.");
+
+        // Check if a .sln file exists in the current directory
+        var solutionFiles = Directory.GetFiles (startDirectory, "*.sln", SearchOption.TopDirectoryOnly);
+        
+		return solutionFiles.Length > 0 
+		? solutionFiles.First () 
+		: FindSolutionPath (Directory.GetParent (startDirectory)!.FullName);
+	}
+}
