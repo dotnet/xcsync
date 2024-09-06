3ad7143976c4694a395a5d9574f6dbeaf2cde5e9
Haritha Mohan <harithamohan@microsoft.com>
2024-09-06 13:38:31 -0400

    use proper casing

--- src/xcsync/Workers/ChangeWorker.cs
+++ src/xcsync/Workers/ChangeWorker.cs
@@ -11,14 +11,14 @@ namespace xcsync;
 
 record struct ChangeMessage (string Id, string Path, SyncDirection Direction, ProjectFileChangeMonitor ClrMonitor, ProjectFileChangeMonitor XcodeMonitor);
 
-class ChangeWorker (IFileSystem fileSystem, ITypeService typeService, string projectPath, string targetDir, string framework, ILogger logger, ClrProject clrProject, XcodeWorkspace xcodeProject) : BaseWorker<ChangeMessage> {
+class ChangeWorker (IFileSystem FileSystem, ITypeService TypeService, string ProjectPath, string TargetDir, string Framework, ILogger Logger, ClrProject ClrProject, XcodeWorkspace XcodeProject) : BaseWorker<ChangeMessage> {
 	public override async Task ConsumeAsync (ChangeMessage message, CancellationToken cancellationToken = default)
 	{
 		message.ClrMonitor.StopMonitoring ();
 		message.XcodeMonitor.StopMonitoring ();
-		await new SyncContext (fileSystem, typeService, message.Direction, projectPath, targetDir, framework, logger).SyncAsync (cancellationToken);
-		message.ClrMonitor.StartMonitoring (clrProject, cancellationToken);
-		message.XcodeMonitor.StartMonitoring (xcodeProject, cancellationToken);
+		await new SyncContext (FileSystem, TypeService, message.Direction, ProjectPath, TargetDir, Framework, Logger).SyncAsync (cancellationToken);
+		message.ClrMonitor.StartMonitoring (ClrProject, cancellationToken);
+		message.XcodeMonitor.StartMonitoring (XcodeProject, cancellationToken);
 	}
 
 	public override Task ConsumeAsync (ChangeMessage message, Exception exception, CancellationToken token = default) 
