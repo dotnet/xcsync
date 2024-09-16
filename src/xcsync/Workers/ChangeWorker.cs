f668dd733ee7d3a9b47e16e1c92c974d93c5d54f
Haritha Mohan <harithamohan@microsoft.com>
2024-09-16 11:22:41 -0400

    localiz

--- src/xcsync/Workers/ChangeWorker.cs
+++ src/xcsync/Workers/ChangeWorker.cs
@@ -14,19 +14,19 @@ record struct ChangeMessage (string Id, string Path, SyncDirection Direction, Pr
 class ChangeWorker (IFileSystem FileSystem, ITypeService TypeService, string ProjectPath, string TargetDir, string Framework, ILogger Logger, ClrProject ClrProject, XcodeWorkspace XcodeProject) : BaseWorker<ChangeMessage> {
 	public override async Task ConsumeAsync (ChangeMessage message, CancellationToken cancellationToken = default)
 	{
-		Logger.Debug ("Pausing monitoring");
+		Logger.Debug (Strings.Watch.PausingMonitoring);
 		message.ClrMonitor.StopMonitoring ();
 		message.XcodeMonitor.StopMonitoring ();
-		Logger.Debug ("Syncing change context");
+		Logger.Debug (Strings.Watch.Syncing);
 		await new SyncContext (FileSystem, TypeService, message.Direction, ProjectPath, TargetDir, Framework, Logger).SyncAsync (cancellationToken);
-		Logger.Debug ("Context synced. Resuming monitoring");
+		Logger.Debug (Strings.Watch.ResumingMonitoring);
 		message.ClrMonitor.StartMonitoring (ClrProject, cancellationToken);
 		message.XcodeMonitor.StartMonitoring (XcodeProject, cancellationToken);
 	}
 
 	public override Task ConsumeAsync (ChangeMessage message, Exception exception, CancellationToken token = default)
 	{
-		Logger.Error (exception, "Error processing change message {Id}: {Message}", message.Id, exception.Message);
+		Logger.Error (exception, Strings.Watch.WorkerException (message.Id, exception.Message));
 		//TODO: if exception is encountered, need to do a better job of alerting and loudly stopping
 		// not a quiet failure and seeminlgy try to continue
 		return Task.CompletedTask;
