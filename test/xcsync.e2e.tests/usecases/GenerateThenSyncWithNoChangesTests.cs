275197d5c1f6f320b077346fc3b46d97c01fb287
Haritha Mohan <harithamohan@microsoft.com>
2024-10-09 12:21:04 -0700

    ensure full project path is used (#125)
    * fixes https://github.com/dotnet/xcsync/issues/123

--- test/xcsync.e2e.tests/usecases/GenerateThenSyncWithNoChangesTests.cs
+++ test/xcsync.e2e.tests/usecases/GenerateThenSyncWithNoChangesTests.cs
@@ -54,6 +54,54 @@ public partial class GenerateThenSyncWithNoChangesTests (ITestOutputHelper testO
 
 
 	[Theory]
+	[InlineData ("macos", "net8.0-macos")]
+	[InlineData ("maccatalyst", "net8.0-maccatalyst")]
+	[InlineData ("ios", "net8.0-ios")]
+	[InlineData ("tvos", "net8.0-tvos")]
+	[InlineData ("maui", "net8.0-ios")]
+	[InlineData ("maui", "net8.0-maccatalyst")]
+	[Trait ("Category", "IntegrationTest")]
+	// Issue #123 : https://github.com/dotnet/xcsync/issues/123
+	public async Task GenerateThenSync_WhenPwdIsProjectDir_DoesNotThrow (string projectType, string tfm)
+	{
+		// Arrange
+
+		var projectName = Guid.NewGuid ().ToString ();
+
+		var tmpDir = Cache.CreateTemporaryDirectory (projectName);
+
+		var xcodeDir = Path.Combine ("obj", "xcode");
+
+		Directory.CreateDirectory (Path.Combine (tmpDir, xcodeDir));
+
+		var csproj = $"{projectName}.csproj";
+
+		await Git (TestOutput, "init", tmpDir).ConfigureAwait (false);
+		await DotnetNew (TestOutput, "gitignore", tmpDir, string.Empty).ConfigureAwait (false);
+		await DotnetNew (TestOutput, "editorconfig", tmpDir, string.Empty).ConfigureAwait (false);
+		await DotnetNew (TestOutput, "nugetconfig", tmpDir, string.Empty).ConfigureAwait (false);
+		await DotnetNew (TestOutput, projectType, tmpDir, string.Empty).ConfigureAwait (false);
+		await DotnetFormat (TestOutput, tmpDir).ConfigureAwait (false);
+		await Git (TestOutput, "-C", tmpDir, "add", ".").ConfigureAwait (false);
+		await Git (TestOutput, "-C", tmpDir, "commit", "-m", "Initial commit").ConfigureAwait (false);
+
+		// Act
+		var originalDirectory = Directory.GetCurrentDirectory ();
+		Directory.SetCurrentDirectory (tmpDir);
+
+		await Xcsync (TestOutput, "generate", "--project", csproj, "--target", xcodeDir, "-tfm", tfm).ConfigureAwait (false);
+		await Xcsync (TestOutput, "sync", "--project", csproj, "--target", xcodeDir, "-tfm", tfm).ConfigureAwait (false);
+
+		Directory.SetCurrentDirectory (originalDirectory);
+		// Assert
+		var commandOutput = new CaptureOutput (TestOutput);
+		var changesPresent = await Git (commandOutput, "-C", tmpDir, "diff", "-w", "--exit-code", "--").ConfigureAwait (false);
+		if (changesPresent == 1)
+			Assert.Fail ($"Git diff failed, there are changes in the source files.\n{commandOutput.Output}");
+	}
+
+
+	[Theory]
 	[InlineData ("https://github.com/haritha-mohan/vision-analyzer", new [] { "VisionAnalyzer/VisionAnalyzer.csproj" })]
 	[Trait ("Category", "IntegrationTest")]
 	public async Task GenerateThenSyncFromGitRepo_WithNoChanges_GeneratesNoChangesAsync (string repoUrl, string [] projects)
