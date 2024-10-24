8fdbb149f23ad691703102370b974b899a5cd83a
Michael Cummings <mcumming@microsoft.com>
2024-10-24 09:27:22 -0400

    Add PbxObject, PbxProjectFile, and PbxPDictionaryExtensions + tests

---
+++ src/xcsync/Projects/Xcode/PbxObject.cs
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Xamarin.MacDev;
+
+namespace xcsync.Projects.Xcode.Model;
+
+record class PbxObject(PbxProjectFile ProjectFile, PbxGuid Guid, PDictionary Properties)
+{
+	public PbxObject? Parent { get; internal set; }
+
+	public PbxObject(PbxProjectFile projectFile) : this(projectFile, PbxGuid.NewGuid(), []) { }
+
+}
