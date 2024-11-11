1eb628429ca2e5d0ea5f77cec2e3727ea4637604
Michael Cummings <mcumming@microsoft.com>
2024-11-11 14:19:38 -0800

    First watch test + passes

--- src/xcsync/Commands/WatchCommand.cs
+++ src/xcsync/Commands/WatchCommand.cs
@@ -1,15 +1,16 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.Collections.Concurrent;
 using System.CommandLine;
 using System.IO.Abstractions;
+using System.Runtime.InteropServices;
 using Serilog;
 using xcsync.Projects;
 
 namespace xcsync.Commands;
 
 class WatchCommand : XcodeCommand<WatchCommand> {
-	bool keepRunning = true;
 
 	public WatchCommand (IFileSystem fileSystem, ILogger logger) : base (fileSystem, logger, "watch", Strings.Commands.WatchDescription)
 	{
@@ -18,36 +19,83 @@ class WatchCommand : XcodeCommand<WatchCommand> {
 
 	public async Task Execute (string project, string target, string tfm, bool force, bool open)
 	{
+		using var cts = new CancellationTokenSource ();
+
+		// Occurs when Ctrl+C or Ctrl+Break is pressed.
 		Console.CancelKeyPress += delegate (object? sender, ConsoleCancelEventArgs e)
 		{
-			e.Cancel = true;
-			keepRunning = false;
+			cts.Cancel ();
+			LogInformation (Strings.Watch.ReceivedCtrlC);
+			e.Cancel = false;
 		};
 
-		LogInformation ("Continuously syncing files between project '{projectPath}' and target '{targetPath} for '{tfm}' platform, press [ESC] to end.", ProjectPath, TargetPath, Tfm);
-		using var cts = new CancellationTokenSource ();
+		LogInformation (Strings.Watch.HeaderInformation(ProjectPath, TargetPath, Tfm));
 
 		var sync = new ContinuousSyncContext (fileSystem, new TypeService (Logger!), ProjectPath, TargetPath, Tfm, Logger!);
 
 		// Start an asynchronous task
-		var task = Task.Run (async () => {
+		var xcsyncTask = Task.Run (async () => {
 			try {
+				if (cts.IsCancellationRequested)
+					return;
 				await sync.SyncAsync (cts.Token).ConfigureAwait (false);
 			} catch (OperationCanceledException) {
-				LogInformation ("Stopping synchronization and completing remaining jobs.");
+				LogInformation (Strings.Watch.StopSynchronization);
 			}
-		});
+		}, cts.Token);
+
+		LogInformation (Strings.Watch.ExitWatchInstructions (!Console.IsInputRedirected) );
+		var consoleReader = new ConsoleReader (cts.Token);
+		consoleReader.Start ();
 
 		// Wait for user input (e.g., ESC key) to cancel the task
-		do {
-			keepRunning = !Console.KeyAvailable && Console.ReadKey (true).Key != ConsoleKey.Escape;
-			await Task.Delay (500);
-		} while (keepRunning);
+		while (cts.IsCancellationRequested == false) {
+			if (consoleReader.Next == (int) ConsoleKey.Escape) {
+				LogInformation (Strings.Watch.ReceivedEsc);
+				cts.Cancel ();
+			}
+		}
 
-		// Cancel the task
-		cts.Cancel ();
+		await xcsyncTask;
+	}
 
-		// Wait for the task to complete (optional)
-		await task;
+	class ConsoleReader {
+		readonly BlockingCollection<int> buffer = new (1);
+		readonly Task readTask;
+		readonly CancellationToken token;
+
+		public ConsoleReader (CancellationToken token)
+		{
+			this.token = token;
+			readTask = new Task (() => {
+				if (Console.IsInputRedirected) {
+					int i;
+					do {
+						i = Console.Read ();
+						buffer.Add (i);
+					} while (i != -1 && token.IsCancellationRequested == false);
+				} else {
+					while (token.IsCancellationRequested == false) {
+						var consoleKeyInfo = Console.ReadKey (true);
+						if (consoleKeyInfo.KeyChar == 0) continue;  // ignore dead keys
+						buffer.Add (consoleKeyInfo.KeyChar);
+					}
+				}
+			}, token);
+		}
+
+		public void Start ()
+		{
+			readTask.Start ();
+		}
+
+		public int? Next {
+			get {
+				if (!token.IsCancellationRequested)
+					return buffer.TryTake (out int result, 0) ? result : default (int?);
+				else
+					return default (int?);
+			}
+		}
 	}
 }
